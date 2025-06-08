18ec8df00bd857571e4191f22e357b0bdef2500a
Mwema22 <stacybeldina50@gmail.com>
2025-06-08 17:14:25 +0300

    moved secret_ket and debug to .env

--- TicketSystem/TicketSystem/settings.py
+++ TicketSystem/TicketSystem/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--9l2pfdd+^sp273r47x812ld(0sa*a@&+grumq!*!(ht$wds*9'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
