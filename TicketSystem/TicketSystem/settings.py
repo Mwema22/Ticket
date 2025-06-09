bccb0951e8bea7b69a8bc34868b8f69413d6696f
Mwema22 <stacybeldina50@gmail.com>
2025-06-09 13:12:03 +0300

    added "fontawesomefree" to installed apps

--- TicketSystem/TicketSystem/settings.py
+++ TicketSystem/TicketSystem/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'fontawesomefree',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
