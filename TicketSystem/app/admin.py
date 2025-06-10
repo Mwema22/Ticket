fcc847256e9ac1bee881c019fed24b9671fe1d8f
Mwema22 <stacybeldina50@gmail.com>
2025-06-10 09:25:18 +0300

    Registered athe models in models.py to admin.py

--- TicketSystem/app/admin.py
+++ TicketSystem/app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import *
+
+admin.site.register(Users),
+admin.site.register(Events),
+admin.site.register(Categories),
+admin.site.register(EventPlanners),
+admin.site.register(Orders),
+admin.site.register(OrderItems),
+admin.site.register(Ticket),
+admin.site.register(TicketTypes),
+admin.site.register(Payments),
+
+
