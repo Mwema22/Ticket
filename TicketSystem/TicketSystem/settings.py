8ba48b41926b4d6f76fc60b3d9003cb7f2332b8e
Mwema22 <stacybeldina50@gmail.com>
2025-06-10 22:00:57 +0300

    Changed "Main" folder to "main" in Templates,  added  STATICFILES_DIRS  for static folder in settings.py

--- TicketSystem/TicketSystem/settings.py
+++ TicketSystem/TicketSystem/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [ 
-                   BASE_DIR / 'Main' / 'templates',# Added my 'Main' folder here
+                   BASE_DIR / 'main' / 'templates',# Added my 'Main' folder here
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -123,9 +123,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
