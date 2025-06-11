9abb4b8c1a81ead1edbb396c7c7178e5c23c7d76
Mwema22 <stacybeldina50@gmail.com>
2025-06-11 23:57:03 +0300

    Added user authentication function in views.py

--- TicketSystem/app/views.py
+++ TicketSystem/app/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
     return render(request,"main/home.html")
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        
+        else:
+            messages.error(request, 'invalid username or password')
+
+    return render(request, 'main/login.html')
+        
